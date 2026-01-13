c137291f7df3fe7724dbba28334fb0b167751423
Lucas Yang <yangchenshin77@gmail.com>
2026-01-13 15:21:19 +0800

    Except CSRF token validation for payment routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '/payment/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
